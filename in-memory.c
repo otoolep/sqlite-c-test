c957d86fcfd358451210a910e68d2d46c4c1023b
Philip O'Toole <philip.otoole@yahoo.com>
2022-12-01 21:58:19 -0500

    Implement threaded version

--- in-memory.c
+++ in-memory.c
@@ -1,3 +1,4 @@
+/* Compile via 'gcc in-memory.c -pthread -l sqlite3' */
 #include <stdio.h>
 #include <pthread.h>
 #include <sqlite3.h>
@@ -6,9 +7,11 @@ void* insertFn(void *arg) {
     char *err_msg = 0;
     int rc = 0;
     sqlite3 *conn = (sqlite3*)arg;
-    rc = sqlite3_exec(conn, "INSERT INTO logs(entry) VALUES('hello')", 0, 0, &err_msg);
-    if (rc != SQLITE_OK) {  
-        fprintf(stderr, "SQL error: %s\n", err_msg);
+    while(1) {
+        rc = sqlite3_exec(conn, "INSERT INTO logs(entry) VALUES('hello')", 0, 0, &err_msg);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+        }
     }
 }
 
@@ -19,6 +22,7 @@ int main(void) {
     char *err_msg = 0;
     int rc = 0;
     sqlite3_stmt *res;
+    pthread_t thread_id;
 
     rc = sqlite3_open_v2("file:/mydb?mode=rw&vfs=memdb&_txlock=immediate&_fk=false", &rwConn, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI, NULL);
     if (rc != SQLITE_OK) {
@@ -30,26 +34,19 @@ int main(void) {
         sqlite3_free(err_msg);        
         return 1;
     }
-    // rc = sqlite3_exec(conn, "INSERT INTO logs(entry) VALUES('hello')", 0, 0, &err_msg);
-    //  if (rc != SQLITE_OK) {  
-    //     fprintf(stderr, "SQL error: %s\n", err_msg);
-    //     sqlite3_free(err_msg);        
-    //     return 1;
-    // }
 
-    insertFn((void*)rwConn);
+    pthread_create(&thread_id, NULL, insertFn, (void*)rwConn);
 
     rc = sqlite3_open_v2("file:/mydb?mode=ro&vfs=memdb&_txlock=deferred&_fk=false", &roConn, SQLITE_OPEN_READONLY | SQLITE_OPEN_URI, NULL);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "Can't open execute connection: %s\n", sqlite3_errmsg(rwConn));
+        fprintf(stderr, "Can't open execute connection: %s\n", sqlite3_errmsg(roConn));
     }   
     rc = sqlite3_prepare_v2(roConn, "SELECT * FROM logs", -1, &res, 0);    
     if (rc != SQLITE_OK) {
         
         fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(roConn));
         return 1;
-    }    
-    
+    }
     rc = sqlite3_step(res);
     if (rc == SQLITE_ROW) {
         printf("%s\n", sqlite3_column_text(res, 0));
